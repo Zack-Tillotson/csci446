c972f5ccec1ff23fbbf2397f7dff907d8a9cba80
Zack Tillotson <ztillots@gmail.com>
2011-01-30 17:41:39 -0700

    Basic cart done

--- project2/app/controllers/application.rb
+++ project2/app/controllers/application.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery # :secret => 'f1a5556c0741fb3a069e8e5e2386022f'
+  protect_from_forgery :secret => 'f1a5556c0741fb3a069e8e5e2386022f'
   
   # See ActionController::Base for details 
   # Uncomment this to filter the contents of submitted sensitive data parameters
