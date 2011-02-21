abfcc13f54694156c76e2c1a1f6889e943514cd9
Zack Tillotson <ztillots@gmail.com>
2011-02-20 22:13:35 -0700

    fixed author photo migration and functionality

--- project05/app/models/author.rb
+++ project05/app/models/author.rb
@@ -1,2 +1,13 @@
 class Author < ActiveRecord::Base
+
+  validates_presence_of :name
+  validates_attachment_presence :photo
+  validate :no_author_of_ill_repute
+
+  has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+
+private
+  def no_author_of_ill_repute
+    errors.add(:name, 'Sally is not allowed to be an author') if name == "Sally"
+  end
 end
--- project05/db/schema.rb
+++ project05/db/schema.rb
@@ -9,12 +9,20 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110221041220) do
+ActiveRecord::Schema.define(:version => 20110221050041) do
 
   create_table "authors", :force => true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "pic_file_name"
+    t.string   "pic_content_type"
+    t.integer  "pic_file_size"
+    t.datetime "pic_updated_at"
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
   end
 
   create_table "documents", :force => true do |t|
