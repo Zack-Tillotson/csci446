651c7b82611c5017e9447acd8e73d5ea2ce396dc
Zack Tillotson <ztillots@gmail.com>
2011-02-20 22:52:10 -0700

    Final clean up

--- project05/app/helpers/application_helper.rb
+++ project05/app/helpers/application_helper.rb
@@ -1,3 +1,7 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def get_subtitle()
+    @subtitles = ["My cousin works at bit.ly", "what's article in spanish anyways?", "not just for spain", "with a name .com hipsters love"]
+    @subtitles[rand(@subtitles.length)]
+  end
 end
