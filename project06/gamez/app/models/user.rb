f861f2744f4515b80bbc47a0ea91eb1b55372cfd
Zack Tillotson <ztillots@gmail.com>
2011-03-06 12:26:52 -0700

    extra folder being sneaky

--- project06/gamez/app/models/user.rb
+++
@@ -1,5 +0,0 @@
-class User < ActiveRecord::Base
-  acts_as_authentic do |c|
-#    c.my_config_option = my_value # for available options see documentation in: Authlogic::ActsAsAuthentic
-  end # block optional
-end
--- project06/gamez/config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = '2f187fb7d0e8f9fdd26fb9cd2c84689785924e8a898009fc1c1405bea8fe19b60e10eff7b1dc02250e13ad4f60ea176b6c4a271b0a1df4a14383310533063c7a';
